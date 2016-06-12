1e90a3ce02d60185cc33db24336e291a4a1ebf15
Mats Högberg <mats@hgbrg.se>
2016-06-12 22:21:20 +0200

    Now counts commits from all branches, not just master

--- github_commit_streak/functions.py
+++ github_commit_streak/functions.py
@@ -20,19 +20,26 @@ def calculate(api_key, email, verbose=False):
 
     for repo in repos:
         if verbose:
-            print('Fetching commits for {0}...'.format(repo['name']))
+            print('Fetching branches for {0}...'.format(repo['name']))
 
-        commits_url = repo['commits_url'].replace('{/sha}', '') + '?author=' + email
-        commits = json_request(commits_url, auth_header)
+        branch_url = repo['branches_url'].replace('{/branch}', '')
+        branches = json_request(branch_url, auth_header)
 
-        for commit in commits:
-            try:
-                if len(commit['parents']) <= 1:
-                    date = commit['commit']['author']['date'][:10]
-                    dates_with_commits.add(date)
-            except TypeError as e:
-                # Something missing from commit, disregard it.
-                pass
+        for branch in branches:
+            if verbose:
+                print('Fetching commits for {0}...'.format(branch['name']))
+
+            commits_url = '{0}?sha={1}&author={2}'.format(repo['commits_url'].replace('{/sha}', ''), branch['name'], email)
+            commits = json_request(commits_url, auth_header)
+
+            for commit in commits:
+                try:
+                    if len(commit['parents']) <= 1:
+                        date = commit['commit']['author']['date'][:10]
+                        dates_with_commits.add(date)
+                except TypeError as e:
+                    # Something missing from commit, disregard it.
+                    pass
 
     count = 0
     date = datetime.date.today()
